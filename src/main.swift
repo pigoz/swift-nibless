95cf6604838828f489e56a6d1f01f480293ab326
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2014-06-08 09:42:29 +0200

    use type inference for NS_ENUMs

--- src/main.swift
+++ src/main.swift
@@ -3,20 +3,15 @@ import Cocoa
 let app          = NSApplication.sharedApplication()
 let window_style = NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask
 let rect         = NSMakeRect(100, 100, 400, 400)
-let window       = NSWindow(contentRect:rect,
-                              styleMask:window_style,
-                                backing:NSBackingStoreType.Buffered,
-                                  defer:false)
+let window       = NSWindow(contentRect:rect, styleMask:window_style,
+                                backing:.Buffered, defer:false)
 let delegate     = AppDelegate()
 
 delegate.window = window
 window.makeKeyAndOrderFront(window)
 window.title = "Nibless Swift Window!"
 app.delegate = delegate
-app.setActivationPolicy(NSApplicationActivationPolicy.Regular)
-atexit_b {
-  app.setActivationPolicy(NSApplicationActivationPolicy.Prohibited)
-  return
-}
+app.setActivationPolicy(.Regular)
+atexit_b { app.setActivationPolicy(.Prohibited); return }
 app.activateIgnoringOtherApps(true)
 app.run()
